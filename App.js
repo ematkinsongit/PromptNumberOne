286802984f7c0d7bf9116c6002c3a78404c44eb4
ematkinsongit <90931047+ematkinsongit@users.noreply.github.com>
2022-04-05 15:31:00 -1000

    Update App.js
    I fixed it :))))))

--- App.js
+++ App.js
@@ -1,44 +1,42 @@
 import Form from "./Components/Form";
 import { useState, useEffect } from "react";
 import PostsList from "./Components/PostsList";
-
 function App() {
   const [postList, setPostList] = useState([]);
   const [intervalIdInState, setIntervalIdInState] = useState(null);
- 
   const addPost = (post) => {
-    const storedPostList = localStorage.getItem("storagePostList" || []);
-    const parsedPosts = JSON.parse(storedPostList);
-    const newPostList = [...parsedPosts, post];
+    const currentPosts = localStorage.getItem("storagePostList") || [];
+    const parsedPosts = JSON.parse(currentPosts) || [];
+    const newPostList = [...parsedPosts, post] || [];
     setPostList(newPostList);
     localStorage.setItem("storagePostList", JSON.stringify(newPostList));
   };
-
   useEffect(() => {
-    const data = localStorage.getItem("storagePostList" || []);
+    const data = localStorage.getItem("storagePostList");
     if (data) {
-      setPostList(JSON.parse(data || []));
+      setPostList(JSON.parse(data));
+    } else {
+      const onLoadPostList = localStorage.setItem(
+        "storagePostList",
+        JSON.stringify([])
+      );
+      setPostList([]);
     }
 
     if (intervalIdInState !== null) {
       clearInterval(intervalIdInState);
     }
-    const intervalId = setInterval(
-      () => {
-        const updatedPosts = localStorage.getItem("storagePostList" || []);
-        if (updatedPosts) {
-          setPostList(JSON.parse(updatedPosts));
-        }
-      },
-
-      10000
-    );
+    const intervalId = setInterval(() => {
+      const updatedPosts = localStorage.getItem("storagePostList");
+      if (updatedPosts) {
+        setPostList(JSON.parse(updatedPosts));
+      }
+    }, 10000);
     setIntervalIdInState(intervalId);
     return () => {
       clearInterval(intervalIdInState);
     };
   }, []);
-
   return (
     <div className="App">
       <Form addPost={addPost} />
@@ -46,5 +44,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
